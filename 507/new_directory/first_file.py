2d493dac6edd28676b46aec2288c0c722348e6fe
californiasky123 <caroline.n.hodge@gmail.com>
2018-02-12 08:45:04 -0500

    added some stuff on feature vv2

--- 507/new_directory/first_file.py
+++ 507/new_directory/first_file.py
@@ -14,3 +14,12 @@ print ("I'm making changes on GitHub and seeing if I can get them locally.")
 Favorite food: Pizza
 Favorite type of music: Rock 
 Sign: Leo 
+
+--- NEW FEATURE -- 
+
+now I'm on my new feature branch 
+
+------------------
+
+NEW FEATURE V2
+- asdflkmasdflkmasdflkm
