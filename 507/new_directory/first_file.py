5112cca4f89f05734deb4cf6ea2606d1582a14ad
californiasky123 <35750679+californiasky123@users.noreply.github.com>
2018-02-11 21:56:24 -0500

    Update first_file.py

--- 507/new_directory/first_file.py
+++ 507/new_directory/first_file.py
@@ -6,3 +6,5 @@ print("here's a third line of code")
 
 
 print ("... making some changes and seeing if I can get them on github")
+
+print ("I'm making changes on GitHub and seeing if I can get them locally.")
