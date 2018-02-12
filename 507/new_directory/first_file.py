d76231eb6f55023bbf563ceb1b67ca7818bc8e08
californiasky123 <caroline.n.hodge@gmail.com>
2018-02-12 08:32:24 -0500

    added a few lines about me

--- 507/new_directory/first_file.py
+++ 507/new_directory/first_file.py
@@ -8,3 +8,9 @@ print("here's a third line of code")
 print ("... making some changes and seeing if I can get them on github")
 
 print ("I'm making changes on GitHub and seeing if I can get them locally.")
+
+
+
+Favorite food: Pizza
+Favorite type of music: Rock 
+Sign: Leo 
